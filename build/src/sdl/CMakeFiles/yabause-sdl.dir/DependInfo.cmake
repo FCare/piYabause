f54cf0c4d0064ed36834eedc43dc7f2830d9c979
Francois CARON <francois.caron.perso@gmail.com>
2017-02-20 23:24:55 +0100

    First alignement on yabause context reference mechanism

---
+++ build/src/sdl/CMakeFiles/yabause-sdl.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/common/piYabause/yabause/src/sdl/main.c" "/common/piYabause/build/src/sdl/CMakeFiles/yabause-sdl.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARCH_IS_LINUX=1"
+  "HAVE_BUILTIN_BSWAP16=1"
+  "HAVE_BUILTIN_BSWAP32=1"
+  "HAVE_C68K=1"
+  "HAVE_C99_VARIADIC_MACROS=1"
+  "HAVE_FLOORF=1"
+  "HAVE_GETTIMEOFDAY=1"
+  "HAVE_GLXGETPROCADDRESS=1"
+  "HAVE_LIBGL=1"
+  "HAVE_LIBGLUT=1"
+  "HAVE_LIBSDL2=1"
+  "HAVE_LIBSDL=1"
+  "HAVE_LIBXRANDR=1"
+  "HAVE_MUSASHI=1"
+  "HAVE_STDINT_H=1"
+  "HAVE_STRCASECMP=1"
+  "HAVE_SYS_TIME_H=1"
+  "IMPROVED_SAVESTATES"
+  "LINUX_CDROM_H_IS_BROKEN"
+  "PACKAGE=\"yabause\""
+  "SH2_TRACE=1"
+  "VERSION=\"0.4.1\""
+  "YAB_PORT_OSD"
+  "YAB_WANT_SSF=1"
+  "_OGL3_"
+  "_OGLES3_"
+  "_USEGLEW_"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/common/piYabause/yabause/src/nanovg"
+  "/common/piYabause/yabause/src/."
+  "/common/piYabause/yabause/src/./nanovg"
+  "/usr/local/include/SDL2"
+  "src/c68k"
+  "src/musashi"
+  "/common/piYabause/yabause/src/musashi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/common/piYabause/build/src/CMakeFiles/yabause.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
