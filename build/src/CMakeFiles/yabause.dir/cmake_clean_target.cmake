b94c1af2cfb780a00b6a2422df0b18c658f1f8e6
Francois CARON <francois.caron.perso@gmail.com>
2017-02-22 21:49:18 +0100

    Do not add those files to git

--- build/src/CMakeFiles/yabause.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libyabause.a"
-)
--- build/src/config.h
+++
@@ -1,6 +0,0 @@
-#define HAVE_C68K 1
-/* #undef HAVE_Q68 */
-/* #undef SH2_DYNAREC */
-#define HAVE_OPENSL 1
-#define YAB_VERSION "0.4.1"
-#define HAVE_MUSASHI 1
